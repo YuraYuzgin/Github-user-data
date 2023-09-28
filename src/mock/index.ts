ddc87aa7e00fb9701ccd0a41b843fda96ee69fe6
Yuriy <yura.yuzgin@yandex.ru>
2023-09-28 22:54:25 +0300

    Сделан вывод всей информации.

---
+++ src/mock/index.ts
@@ -0,0 +1,17 @@
+import { LocalGithubUser } from 'types';
+
+export const defaultUser: LocalGithubUser = {
+  login: 'YuraYuzgin',
+  id: 39434769,
+  avatar: 'https://avatars.githubusercontent.com/u/39434769?v=4',
+  name: 'Yuriy',
+  company: null,
+  blog: '',
+  location: 'Vologda',
+  bio: null,
+  twitter: null,
+  repos: 10,
+  followers: 0,
+  following: 0,
+  created: '2018-05-19T11:38:34Z',
+};
